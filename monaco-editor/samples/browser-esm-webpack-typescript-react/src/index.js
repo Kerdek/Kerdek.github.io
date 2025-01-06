74b05db08326626381ce7f498c2d4e0fbeb23c14
ted <kerdek7@gmail.com>
2025-01-06 09:18:54 -0500

    updated gitignore

--- monaco-editor/samples/browser-esm-webpack-typescript-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Editor } from './components/Editor';
-ReactDOM.render(<React.StrictMode>
-		<Editor />
-	</React.StrictMode>, document.getElementById('root'));
-//# sourceMappingURL=index.js.map
